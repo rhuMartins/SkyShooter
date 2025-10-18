1c1e08fe9805021f826ff8db5dde06988707ecc5
rhuMartins <martinsrhuan99@gmail.com>
2025-10-17 23:17:09 -0300

    SQLite 3 implementation for Score using proxy design pattern

--- code/Const.py
+++ code/Const.py
@@ -1,5 +1,6 @@
 # C
 import pygame
+from pygame.examples.grid import WINDOW_WIDTH, WINDOW_HEIGHT
 
 C_BLACK = (0, 0, 0)
 C_BLUE = (76, 121, 146)
@@ -29,8 +30,7 @@ ENTITY_DAMAGE = {'Level1BG0': 0,
                  'Enemy1Shot': 20,
                  'Enemy2': 1,
                  'Enemy2Shot': 15,
-
-}
+                 }
 
 ENTITY_HEALTH = {'Level1BG0': 999,
                  'Level1BG1': 999,
@@ -50,7 +50,8 @@ ENTITY_HEALTH = {'Level1BG0': 999,
                  'Enemy1': 50,
                  'Enemy1Shot': 1,
                  'Enemy2': 60,
-                 'Enemy2Shot': 1,}
+                 'Enemy2Shot': 1,
+                 }
 
 ENTITY_SCORE = {'Level1BG0': 0,
                 'Level1BG1': 0,
@@ -70,12 +71,14 @@ ENTITY_SCORE = {'Level1BG0': 0,
                 'Enemy1': 100,
                 'Enemy1Shot': 0,
                 'Enemy2': 125,
-                'Enemy2Shot': 0,}
+                'Enemy2Shot': 0,
+                }
 
 ENTITY_SHOT_DELAY = {'Player1': 40,
                      'Player2': 20,
                      'Enemy1': 50,
-                     'Enemy2': 200}
+                     'Enemy2': 200
+                     }
 
 ENTITY_SPEED = {'Level1BG0': 0,
                 'Level1BG1': 2,
@@ -95,7 +98,8 @@ ENTITY_SPEED = {'Level1BG0': 0,
                 'Enemy1': 2,
                 'Enemy1Shot': 5,
                 'Enemy2': 1,
-                'Enemy2Shot': 2,}
+                'Enemy2Shot': 2,
+                }
 
 EVENT_ENEMY = pygame.USEREVENT + 1
 EVENT_TIMEOUT = pygame.USEREVENT + 2
@@ -105,25 +109,42 @@ MENU_OPTION = ('NEW GAME 1 PLAYER',
                'NEW GAME 2 PLAYERS - COOPERATIVE',
                'NEW GAME 2 PLAYERS - COMPETITIVE',
                'SCORE',
-               'EXIT')
+               'EXIT'
+               )
 
 # P
 PLAYER_KEY_UP = {'Player1': pygame.K_w,
                  'Player2': pygame.K_UP}
 PLAYER_KEY_DOWN = {'Player1': pygame.K_s,
-                 'Player2': pygame.K_DOWN}
+                   'Player2': pygame.K_DOWN}
 PLAYER_KEY_LEFT = {'Player1': pygame.K_a,
-                 'Player2': pygame.K_LEFT}
+                   'Player2': pygame.K_LEFT}
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_d,
-                 'Player2': pygame.K_RIGHT}
+                    'Player2': pygame.K_RIGHT}
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_LCTRL,
                     'Player2': pygame.K_RCTRL}
 
 # S
+SCORE_POS = {'Title': (WINDOW_HEIGHT / 2, 50),
+             'EnterName': (WINDOW_WIDTH / 2, 80),
+             'Label': (WINDOW_WIDTH / 2, 90),
+             'Name': (WINDOW_WIDTH / 2, 110),
+             0: (WINDOW_WIDTH / 2, 110),
+             1: (WINDOW_WIDTH / 2, 130),
+             2: (WINDOW_WIDTH / 2, 150),
+             3: (WINDOW_WIDTH / 2, 170),
+             4: (WINDOW_WIDTH / 2, 190),
+             5: (WINDOW_WIDTH / 2, 210),
+             6: (WINDOW_WIDTH / 2, 230),
+             7: (WINDOW_WIDTH / 2, 250),
+             8: (WINDOW_WIDTH / 2, 270),
+             9: (WINDOW_WIDTH / 2, 290),
+             }
+
 SPAWN_TIME = 2500
 
 # T
-TIMEOUT_LEVEL = 30000
+TIMEOUT_LEVEL = 20000
 TIMEOUT_STEP = 100
 
 # W
